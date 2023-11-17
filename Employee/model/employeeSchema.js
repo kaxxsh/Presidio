887bc0023362e56eb928d580c71d57c8a72a6a43
kaxxsh <kameshwaran2002@gmail.com>
2023-11-18 00:46:02 +0530

    schema screation

--- Employee/model/employeeSchema.js
+++ Employee/model/employeeSchema.js
@@ -1,1 +1,33 @@
+import mongoose from "mongoose";
 
+const Employee = mongoose.Schema({
+  Name: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  age: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  dob: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  salery: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  department: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+});
+
+const EmployeeSchema = mongoose.model("Employee", Employee);
+
+export default EmployeeSchema;
