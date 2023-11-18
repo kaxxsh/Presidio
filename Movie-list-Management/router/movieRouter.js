a2020002d105b15a8fc728a83718194a265074c0
kaxxsh <kameshwaran2002@gmail.com>
2023-11-18 22:31:01 +0530

    Movie:Router update

---
+++ Movie-list-Management/router/movieRouter.js
@@ -0,0 +1,18 @@
+import Express from "express";
+import {
+  allMovie,
+  addMovie,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+} from "../controller/MovieController.js";
+
+const Router = Express.Router();
+
+// Route: GET /, POST /
+Router.route("/").get(allMovie).post(addMovie);
+
+// Route: GET /:id, PATCH /:id, DELETE /:id
+Router.route("/:id").get(getMovie).patch(updateMovie).delete(deleteMovie);
+
+export default Router;
