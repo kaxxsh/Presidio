049e7023a3a1fdcb3ef88ebc089fd247658a3408
kaxxsh <kameshwaran2002@gmail.com>
2023-11-18 13:40:08 +0530

    creating custom error middleware

---
+++ Employee/middleware/error-handler.js
@@ -0,0 +1,10 @@
+const handleError = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || 404,
+    message: err.message || "Something went wrong",
+  };
+
+  res.status(customError.statusCode).json({ message: customError.message });
+};
+
+export default handleError;
