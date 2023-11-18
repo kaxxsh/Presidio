4e036f65920bc7f3371c0fd4f5fce29ceba12b77
kaxxsh <kameshwaran2002@gmail.com>
2023-11-18 22:20:43 +0530

    creating movie list

---
+++ Movie-list-Management/app.js
@@ -0,0 +1,23 @@
+import Express from "express";
+import dbconnection from "./connection/dbConnection.js";
+import {} from "dotenv/config.js";
+
+
+// Create an instance of the Express application
+const app = Express();
+app.use(Express.json());
+
+
+// Function to connect to the database and start the server
+const connect = async () => {
+  try {
+    dbconnection(process.env.MONGO_URL);
+    app.listen(process.env.PORT, () =>
+      console.log(`listerning at ${process.env.PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
